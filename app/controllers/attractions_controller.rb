6c5a8e0ac5a47e783554b0b057c98abe1fcb2806
github username <github email address>
2017-06-15 12:58:00 -0400

    first edits

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,13 @@
+class AttractionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
 
+  
+  get 'attractions/new'
+  get 'attractions/create'
+  get 'attractions/show'
+  get 'attractions/index'
+
+  root 'users#homepage'
 
 end
